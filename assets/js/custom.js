81a069305d5d285656df979a7959f5496b05f253
Parag <parag-arora@paragarora.com>
2017-09-09 23:46:43 +0530

    landing page

---
+++ assets/js/custom.js
@@ -0,0 +1,247 @@
+/* ----- Custom Scripts for Pilot template ----- */
+
+jQuery(function($) {
+  "use strict";
+
+  // get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
+    var mainbottom = $('#main').offset().top;
+    // on scroll,
+    $(window).on('scroll', function(){
+    // we round here to reduce a little workload
+    stop = Math.round($(window).scrollTop());
+    if (stop > mainbottom) {
+        console.log("ADD");
+        $('#inlineicon').show();
+        $('.navbar').addClass('past-main');
+        $('.navbar').addClass('effect-main')
+    } else {
+        console.log("REMOVE");
+        $('#inlineicon').hide();
+        $('.navbar').removeClass('past-main');
+      }
+    });
+
+  // Collapse navbar on click
+
+  $(document).on('click.nav','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+    $(this).removeClass('in').addClass('collapse');
+   }
+  });
+
+/*----- Preloader ----- */
+
+  $(window).load(function() {
+		setTimeout(function() {
+      $('#loading').fadeOut('slow', function() {
+      });
+    }, 300);
+  });
+
+/* --------- Wow Init -------*/
+
+  new WOW().init();
+
+/* ------- Magnific Popup ---------*/
+
+  $('.popup').magnificPopup({
+    disableOn: 0,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
+  });
+
+/*-------- Owl Carousel ---------- */
+
+  $(".reviews").owlCarousel({
+
+    slideSpeed : 200,
+    items: 1,
+    singleItem: true,
+    autoPlay : true,
+    pagination : false
+  });
+
+/* ------ Clients Section Owl Carousel ----- */
+
+  $(".clients").owlCarousel({
+    slideSpeed : 200,
+    items: 5,
+    singleItem: false,
+    autoPlay : true,
+    pagination : false
+  });
+
+
+/*----------- Scroll To Top ---------------*/
+
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 1000) {
+        $('#back-top').fadeIn();
+    } else {
+        $('#back-top').fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $('#back-top').on('click', function () {
+    $('#back-top').tooltip('hide');
+    $('body,html').animate({
+        scrollTop: 0
+    }, 1500);
+    return false;
+  });
+
+/* ------ Countdown ----- */
+
+  $('#countdown').countdown({
+		date: '12/07/2017 12:00:00',
+		offset: +2,
+		day: 'Day',
+		days: 'Days'
+	 }, function () {
+		alert('Done!');
+	});
+
+/* ------ jQuery for Easing min -- */
+
+  $(function() {
+  $('a.page-scroll').on('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+    });
+  });
+
+
+  /*----- Subscription Form ----- */
+
+  $(document).ready(function() {
+    // jQuery Validation
+    $("#signup").validate({
+      // if valid, post data via AJAX
+      submitHandler: function(form) {
+        $.post("assets/php/subscribe.php", { email: $("#email").val() }, function(data) {
+          $('#response').html(data);
+        });
+      },
+      // all fields are required
+      rules: {
+        email: {
+          required: true,
+          email: true
+        }
+      }
+    });
+  });
+  });
+
+
+  /*----CANVAS -------*/
+  var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var stars = [], // Array that contains the stars
+    FPS = 180, // Frames per second
+    x = 100, // Number of stars
+    mouse = {
+      x: 0,
+      y: 0
+    };  // mouse location
+
+// Push stars to array
+
+for (var i = 0; i < x; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    radius: Math.random() * 1 + 1,
+    vx: Math.floor(Math.random() * 50) - 25,
+    vy: Math.floor(Math.random() * 50) - 25
+  });
+}
+
+// Draw the scene
+
+function draw() {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+
+  ctx.globalCompositeOperation = "lighter";
+
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var s = stars[i];
+
+    ctx.fillStyle = "#386fa4";
+    ctx.beginPath();
+    ctx.arc(s.x, s.y, s.radius/1.5, 0, 2*Math.PI/1.5);
+    ctx.fill();
+    ctx.fillStyle = 'black';
+    ctx.stroke();
+  }
+
+  ctx.beginPath();
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var starI = stars[i];
+    ctx.moveTo(starI.x,starI.y);
+    if(distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
+    for (var j = 0, x = stars.length; j < x; j++) {
+      var starII = stars[j];
+      if(distance(starI, starII) < 150) {
+        //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
+        ctx.lineTo(starII.x,starII.y);
+      }
+    }
+  }
+  ctx.lineWidth = 0.05;
+  ctx.strokeStyle = '#ADD8E6';
+  ctx.stroke();
+}
+
+function distance( point1, point2 ){
+  var xs = 0;
+  var ys = 0;
+
+  xs = point2.x - point1.x;
+  xs = xs * xs;
+
+  ys = point2.y - point1.y;
+  ys = ys * ys;
+
+  return Math.sqrt( xs + ys );
+}
+
+// Update star locations
+
+function update() {
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var s = stars[i];
+
+    s.x += s.vx / FPS;
+    s.y += s.vy / FPS;
+
+    if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
+    if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
+  }
+}
+
+canvas.addEventListener('mousemove', function(e){
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+});
+
+// Update and draw
+
+function tick() {
+  draw();
+  update();
+  requestAnimationFrame(tick);
+}
+
+tick();
