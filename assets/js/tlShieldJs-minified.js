7dfa7a23cd6cf3d5f0396011059eeb9ae22bab89
SHIVAM SETHI <shivamsethi2201@gmail.com>
2017-11-12 19:09:31 +0530

    Integrate TL Shield widget

---
+++ assets/js/tlShieldJs-minified.js
@@ -0,0 +1,1 @@
+function popUp(){var e=document.getElementById("popup");"none"==e.style.display?e.style.display="block":e.style.display="none"}function popupClose(){var e=document.getElementById("popup");e.style.display="none"}window.onload=function(){document.getElementById("popup").style.display="none"};
